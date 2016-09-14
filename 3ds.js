6b274a78b4bb0e71059be49d258b56ebb425ef28
Takuto Kimura <takuto@heart-s.com>
2016-09-14 13:42:56 +0900

    fixed console fixed old 3ds bottom screen size.

--- 3ds.js
+++ 3ds.js
@@ -9,9 +9,6 @@ var N3 = function() {
   //this.env.ON = true;
   //this.env.NEW = true;
 
-  this.env.console = false;
-  this.con = null;
-
   this.$topScreen = null;
   this.$bottomScreen = null;
 
@@ -34,7 +31,6 @@ var N3 = function() {
   }
 }
 
-N3.prototype.console = function(){};
 
 N3.prototype.init = function(scr) {
  this.$scr = scr === undefined ? $('#screen') : $(scr);
@@ -54,34 +50,14 @@ N3.prototype.init = function(scr) {
     = $('<div style="width:400px;height:224px;overflow:hidden;margin:0 auto;background-color:red;position: relative;">');
   this.$bottomScreen
     = $('<div style="width:320px;height:240px;overflow:hidden;margin:0 auto;background-color:blue;position: relative;">');
-
-  if(this.OLD){
-    this.$bottomScreen.css('height','224px');
+  
+  // old 3ds's bottomscreen is small
+  if(this.env.OLD) {
+    this.$bottomScreen.css('height','212px');
   }
 
   this.$scr.append(this.$topScreen);
   this.$scr.append(this.$bottomScreen);  
-
-  // for console
-  if($("#console")){
-    this.env.console = true;
-    this.$con = $('<textarea name="console" rows="25" cols="42" style="margin:12px 0px 0px 40px"></textarea>');
-    $("#console").hide();
-    $("#console").append(this.$con);
-
-    // コンソール表示切替ボタン
-    var $con_btn = 
-      $('<span style="height:20px;width:20px;position:absolute;top:227px;right:44px;background-color:#ccc;border-radius:20px;-moz-border-radius:20px;-webkit-border-radius:20px;vertical-align: middle;border:3px solid #555"> </span>');
-    $con_btn.bind('click',{_this : this }, function(event){
-        event.data._this.$scr.toggle();
-        $("#console").toggle();
-    }) ;
-    $(document.body).append($con_btn);
-    
-    this.console = function(text){
-      this.$con.append(text + '\n');
-    };
-  }
 
   // 画面固定用
   if(this.env.ON)
@@ -92,4 +68,36 @@ N3.prototype.init = function(scr) {
   }
 };
 
+
+// ログ出力用画面設定
+var _CONSOLE = function(){
+  this.log = function(){};
+
+  if($("#console_log")){
+    var $logdiv = $("#console_log");
+    var $logarea =$('<textarea name="console" rows="25" cols="42" style="margin:12px 0px 0px 40px"></textarea>');
+
+    $logdiv.css("height","464");
+    $logdiv.hide();
+    $logdiv.append($logarea);
+
+    // コンソール表示切替ボタン
+    var $con_btn = 
+      $('<span style="height:20px;width:20px;position:absolute;top:227px;right:44px;background-color:#ccc;border-radius:20px;-moz-border-radius:20px;-webkit-border-radius:20px;vertical-align: middle;border:3px solid #555"> </span>');
+    $con_btn.bind('click',{ _this : this }, function(event){
+        $('#screen').toggle();
+        $logdiv.toggle();
+    }) ;
+    $(document.body).append($con_btn);
+
+    this.log = function(s){
+      $logarea.append(s + '\n');
+    }
+  }
+}
+
+var _console = null;
+$(function(){
+  _console = new _CONSOLE();
+});
 
